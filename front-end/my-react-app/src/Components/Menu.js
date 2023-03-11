82e5f43a6685ad906a69f33f2a579f579df59e40
marialaura <marialaurap27@gmail.com>
2023-03-11 12:29:04 -0300

    Ajustes  finais

--- front-end/my-react-app/src/Components/Menu.js
+++ front-end/my-react-app/src/Components/Menu.js
@@ -5,30 +5,38 @@ export default function Menu() {
     <div className="flex bg-zinc-300">
       <nav>
         <ul className="flex flex-row">
-          <li className="p-5 hover:bg-lime-800 hover:text-slate-50">
+          <li className="hover:bg-lime-800 hover:text-slate-50">
             <Link to="/">
-            Home
+              <div className="p-5">Home</div>
             </Link>
           </li>
           <li className="p-5 group/item relative cursor-pointer hover:bg-lime-800 hover:text-slate-50">
             Times
             <ul className="hidden group-hover/item:block absolute mt-7 top-9 left-0 w-48 bg-zinc-300">
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to="times">Lista de times</Link>
+              <li className="hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to="times">
+                  <div className="p-4 ">Lista de times</div>
+                </Link>
               </li>
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to="criar-time">Criar time</Link>
+              <li className="hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to="criar-time">
+                <div className="p-4 ">Criar time</div>
+                </Link>
               </li>
             </ul>
           </li>
           <li className="p-5 group/item relative cursor-pointer hover:bg-lime-800 hover:text-slate-50">
             Jogadores
             <ul className="hidden group-hover/item:block absolute mt-7 top-9 left-0 w-48 bg-zinc-300">
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to={"jogadores"}>Lista de jogadores</Link>
+              <li className="hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to="jogadores">
+                <div className="p-4 ">Lista de jogadores</div>
+                </Link>
               </li>
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to={"criar-jogador"}>Criar jogador</Link>
+              <li className="hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to="criar-jogador">
+                <div className="p-4">Criar Jogador</div>
+                </Link>
               </li>
             </ul>
           </li>
