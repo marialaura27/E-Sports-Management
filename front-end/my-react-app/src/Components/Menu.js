8236d0d28c4f65947dce18a72480bc66a255aa03
marialaura <marialaurap27@gmail.com>
2023-03-11 10:46:56 -0300

    change menu order

--- front-end/my-react-app/src/Components/Menu.js
+++ front-end/my-react-app/src/Components/Menu.js
@@ -9,17 +9,6 @@ export default function Menu() {
             <Link to="/">
             Home
             </Link>
-            </li>
-          <li className="p-5 group/item relative cursor-pointer hover:bg-lime-800 hover:text-slate-50">
-            Jogadores
-            <ul className="hidden group-hover/item:block absolute mt-7 top-9 left-0 w-48 bg-zinc-300">
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to={"jogadores"}>Lista de jogadores</Link>
-              </li>
-              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
-                <Link to={"criar-jogador"}>Criar jogador</Link>
-              </li>
-            </ul>
           </li>
           <li className="p-5 group/item relative cursor-pointer hover:bg-lime-800 hover:text-slate-50">
             Times
@@ -29,6 +18,17 @@ export default function Menu() {
               </li>
               <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
                 <Link to="criar-time">Criar time</Link>
+              </li>
+            </ul>
+          </li>
+          <li className="p-5 group/item relative cursor-pointer hover:bg-lime-800 hover:text-slate-50">
+            Jogadores
+            <ul className="hidden group-hover/item:block absolute mt-7 top-9 left-0 w-48 bg-zinc-300">
+              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to={"jogadores"}>Lista de jogadores</Link>
+              </li>
+              <li className="p-4 hover:bg-lime-800 text-black hover:text-slate-50">
+                <Link to={"criar-jogador"}>Criar jogador</Link>
               </li>
             </ul>
           </li>
